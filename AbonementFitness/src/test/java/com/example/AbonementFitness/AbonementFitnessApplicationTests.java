bee075b02dacc19eba9334b8b6fbf9a5577d5384
VaryaKurtseva <varyakurtseva@yandex.ru>
2026-03-09 15:12:29 +0300

    Подключение локального репозитория

---
+++ AbonementFitness/src/test/java/com/example/AbonementFitness/AbonementFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AbonementFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbonementFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
