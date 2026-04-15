7027be228d549b061aa510c376a453011765ca91
Мишка <mihagolovin2205@gmail.com>
2026-04-15 16:43:14 +0300

    Добавления заданий по graphql

--- Abonement/Abonement-demo-rest/src/main/java/com/example/Abonement_demo_rest/AbonementDemoRestApplication.java
+++ Abonement/Abonement-demo-rest/src/main/java/com/example/Abonement_demo_rest/AbonementDemoRestApplication.java
@@ -2,18 +2,13 @@ package com.example.Abonement_demo_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication(
 		scanBasePackages = {
 				"com.example.Abonement_demo_rest",
 				"com.example.AbonementFitness"
-		},
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
 		}
 )
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
