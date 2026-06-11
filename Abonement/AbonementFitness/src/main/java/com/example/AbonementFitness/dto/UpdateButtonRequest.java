4e084c0f4b29fe3c24e8e5f58acb03a6b1027f1f
Varyyaass <varyakurtseva@yandex.ru>
2026-06-11 21:46:36 +0300

    Update UpdateButtonRequest.java

--- Abonement/AbonementFitness/src/main/java/com/example/AbonementFitness/dto/UpdateButtonRequest.java
+++ Abonement/AbonementFitness/src/main/java/com/example/AbonementFitness/dto/UpdateButtonRequest.java
@@ -18,20 +18,19 @@ import java.time.LocalDate;
         + "Пользователь кнопки не меняется.")
 public record UpdateButtonRequest (
         @Schema(description = "Сколько посещений")
-        @NotBlank(message = "Количество посещений не может быть пустым")
                 @ValidVisitsHall
         Integer visitsHall,
         @Schema(description = "На сколько месяцев абонемент",  requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank(message = "Длительность абонемента не может быть пустой")
+        @NotNull(message = "Длительность абонемента не может быть пустой")
         @Max(value = 12, message = "Максимум 12 месяцев")
         @Min(value = 1, message = "Минимум 1 месяц")
         Integer value,
 
         @Schema(description = "Дата активации абонемента", example = "2026-01-03")
-        @NotBlank(message = "Дата активации абонемента не может быть пустой")
+        @NotNull(message = "Дата активации абонемента не может быть пустой")
         LocalDate subscriptionActivation,
         @Schema(description = "Дата окончания абонемента", example = "2026-03-03")
-        @NotBlank(message = "Дата окончания абонемента не может быть пустой")
+        @NotNull(message = "Дата окончания абонемента не может быть пустой")
         LocalDate endOfSubscription
 ){}
 
