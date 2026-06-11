ba0e1f4c9e011c88b43008a94769b7e11135eb0d
Varyyaass <varyakurtseva@yandex.ru>
2026-06-11 16:41:03 +0300

    Delete AbonementFitness directory

--- AbonementFitness/src/main/java/com/example/AbonementFitness/exception/ApiButtonExceptionUserId.java
+++
@@ -1,7 +0,0 @@
-package com.example.AbonementFitness.exception;
-
-public class ApiButtonExceptionUserId extends RuntimeException {
-    public ApiButtonExceptionUserId(long userId) {
-        super("У пользователя с id =" + String.valueOf(userId) + " есть активный абонемент");
-    }
-}
--- AbonementFitness/src/main/java/com/example/AbonementFitness/exception/InvalidDateRange.java
+++
@@ -1,7 +0,0 @@
-package com.example.AbonementFitness.exception;
-
-public class InvalidDateRange extends RuntimeException{
-    public InvalidDateRange(long userId) {
-        super("Неккоректный диапозон дат. Дата окончания должна быть позже даты начала");
-    }
-}
